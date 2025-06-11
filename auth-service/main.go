6682ce3a5983dd2c2f4b776bd05012309ccd7ecb
QoidynBasy <c0mplexie1@gmail.com>
2025-06-12 01:44:30 +0500

    docker-compose.yml

--- auth-service/main.go
+++ auth-service/main.go
@@ -4,38 +4,20 @@ import (
 	"auth-service/controllers"
 	"auth-service/models"
 	"auth-service/utils"
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-var db *gorm.DB
-
-func init() {
-	var err error
-	if err := godotenv.Load(); err != nil {
-		log.Println("No .env file found, relying on system environment variables")
-	}
-	dsn := "host=" + utils.GetEnv("DB_HOST", "localhost") +
-		" user=" + utils.GetEnv("DB_USER", "postgres") +
-		" password=" + utils.GetEnv("DB_PASSWORD", "") +
-		" dbname=" + utils.GetEnv("DB_NAME", "mvpdiploma") +
-		" port=" + utils.GetEnv("DB_PORT", "5432") +
-		" sslmode=disable"
-
-	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("AuthService: Failed to connect Database %v", err)
-	}
-	_ = db.AutoMigrate(&models.User{}, &models.UserDatabase{}, &models.PasswordResetToken{}, &models.EmailVerificationToken{})
-}
-
 func main() {
-	r := gin.Default()
+	db := setupDatabase()
 
+	r := gin.Default()
 	userController := controllers.NewAuthController(db)
 
 	auth := r.Group("/auth")
@@ -45,18 +27,37 @@ func main() {
 		auth.POST("/reset-password", userController.ResetPassword)
 		auth.POST("/change-password", userController.ChangePassword)
 		auth.GET("/me", userController.GetMe)
-		auth.POST("/confirm", userController.ConfirmEmail)
-
-		authGoogle := auth.Group("/google")
-		{
-			authGoogle.GET("/", userController.LoginWithGoogle)
-			authGoogle.GET("/callback", userController.GoogleCallback)
-		}
 	}
 
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"status": "ok"})
 	})
 
-	r.Run(utils.GetEnv("PORT", ":5003"))
+	port := os.Getenv("SERVER_PORT")
+	if port == "" {
+		port = "5003"
+	}
+	r.Run(":" + port)
+}
+
+func setupDatabase() *gorm.DB {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		utils.GetEnv("DB_HOST", "localhost"),
+		utils.GetEnv("DB_USER", "postgres"),
+		utils.GetEnv("DB_PASSWORD", ""),
+		utils.GetEnv("DB_NAME", "mvpdiploma"),
+		utils.GetEnv("DB_PORT", "5432"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("AuthService: Failed to connect Database %v", err)
+	}
+
+	err = db.AutoMigrate(&models.User{}, &models.UserDatabase{}, &models.PasswordResetToken{}, &models.EmailVerificationToken{})
+	if err != nil {
+		log.Fatalf("AutoMigrate failed: %v", err)
+	}
+	return db
 }
